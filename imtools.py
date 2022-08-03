5082011dc1bbff6f9bb4624ca06cd7f9e9dc179d
Alexander Muravtsev <almuravcev@yandex.ru>
2022-08-03 23:02:07 +0300

    add numpy array resizer to imtools

--- imtools.py
+++ imtools.py
@@ -1,9 +1,19 @@
 import os
+import numpy as np
+from PIL import Image
 
 def get_imlist(
     path: str
 ) -> list:
     return [os.path.join(path, f) for f in os.listdir(path) if f.endswith('.jpg') and not f.startswith('.')]
+
+
+def arr_resize(
+    arr: np.array,
+    target_size: tuple,
+):
+    pil_im = Image.fromarray(np.uint8(arr))
+    return np.array(pil_im.resize(target_size))
 
 
 if __name__ == '__main__':
