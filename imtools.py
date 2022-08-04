a89d19885bcb55b26c38c847c36f73dbf956a4ef
Alexander Muravtsev <almuravcev@yandex.ru>
2022-08-04 23:38:03 +0300

    add histogram equalization to imtools

--- imtools.py
+++ imtools.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 from PIL import Image
 
+
 def get_imlist(
     path: str
 ) -> list:
@@ -11,9 +12,21 @@ def get_imlist(
 def arr_resize(
     arr: np.array,
     target_size: tuple,
-):
+) -> np.array:
     pil_im = Image.fromarray(np.uint8(arr))
     return np.array(pil_im.resize(target_size))
+
+
+def hist_equalization(
+    arr: np.array,
+    bins_count=256,
+) -> np.array:
+    arr_hist, bins = np.histogram(arr.flatten(), bins_count, normed=True)
+    cdf = arr_hist.cumsum()
+    cdf = 255 * cdf / cdf[-1]
+
+    new_arr = np.interp(arr.flatten(), bins[:-1], cdf)
+    return new_arr.reshape(arr.shape), cdf
 
 
 if __name__ == '__main__':
