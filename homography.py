4974b8ab415f0b00d5a42fbd62e18e129483cda4
Alexander Muravtsev <almuravcev@yandex.ru>
2022-08-11 17:15:34 +0300

    start homography exploration

---
+++ homography.py
@@ -0,0 +1,78 @@
+import numpy as np
+
+
+def normalize(
+    points: np.ndarray,
+) -> np.ndarray:
+    for row in points:
+        row /= points[-1]
+    return points
+
+
+def make_homog(
+    points: np.ndarray
+) -> np.ndarray:
+    return np.vstack((points, np.ones((1, points.shape[1]))))
+
+
+def better_cond(
+    matrix: np.ndarray
+) -> tuple:
+    m = np.mean(matrix[:2], axis=1)
+    max_std = max(np.std(matrix[:2], axis=1)) + 1e-9
+    c = np.diag([1 / max_std, 1 / max_std, 1])
+    c[0][2] = - m[0] / max_std
+    c[1][2] = - m[1] / max_std
+    return np.dot(c, matrix), c
+
+
+def h_from_points(
+    fp: np.ndarray,
+    tp: np.ndarray,
+) -> np.ndarray:
+    assert fp.shape == tp.shape
+
+    # для первого изображения
+    fp, c1 = better_cond(fp)
+    # для второго изображения
+    tp, c2 = better_cond(tp)
+
+    # создание матрицы (по две строки для каждой пары соответственных точек)
+    nbr_correspondences = fp.shape[1]
+    a = np.zeros((2 * nbr_correspondences, 9))
+    # наполнение матрицы
+    for i in range(nbr_correspondences):
+        a[2*i] = [-fp[0][i], -fp[1][i], -1, 0, 0, 0, fp[0][i]*tp[0][i], fp[1][i]*tp[0][i], tp[0][i]]
+        a[2*i+1] = [0, 0, 0, -fp[0][i], -fp[1][i], -1, fp[0][i]*tp[1][i], fp[1][i]*tp[1][i], tp[1][i]]
+    # находим значение H, минимизирующее среднеквадратичную ошибку, применив метод сингулярного разложения (SVD)
+    _, _, v = np.linalg.svd(a)
+    h = v[8].reshape((3, 3))
+
+    # обратное преобразование, компенсирующее обуславливание
+    h = np.dot(np.linalg.inv(c2), np.dot(h, c1))
+
+    return h / h[2, 2]
+
+
+def haffine_from_points(
+    fp: np.ndarray,
+    tp: np.ndarray,
+) -> np.ndarray:
+    assert fp.shape == tp.shape
+
+    # для первого изображения
+    fp, c1 = better_cond(fp)
+    # для второго изображения
+    tp, c2 = better_cond(tp)
+
+    a = np.concatenate((fp[:2], tp[:2]), axis=0)
+    _, _, v = np.linalg.svd(a.T)
+
+    tmp = v[:2].T
+    b = tmp[:2]
+    c = tmp[2:4]
+    tmp2 = np.concatenate((np.dot(c, np.linalg.pinv(b)), np.zeros((2,1))), axis=1)
+    h = np.vstack((tmp2, [0, 0, 1]))
+    h = np.dot(np.linalg.inv(c2), np.dot(h, c1))
+
+    return h / h[2, 2]
---
+++ warp.py
@@ -0,0 +1,33 @@
+import numpy as np
+from scipy import ndimage
+from scipy.spatial import Delaunay
+import homography
+
+
+def image_to_image(
+    im1: np.ndarray,
+    im2: np.ndarray,
+    tp: np.ndarray,
+) -> np.ndarray:
+    m, n = im1.shape[:2]
+    fp = np.array([[0, m, m, 0], [0, 0, n, n], [1, 1, 1, 1]])
+    h = homography.haffine_from_points(tp, fp)
+    im1_transformed = ndimage.affine_transform(im1, h[:2, :2], (h[0, 2], h[1, 2]), im2.shape[:2])
+    alpha = (im1_transformed > 0)
+
+    return (1 - alpha) * im2 + alpha * im1_transformed
+
+
+def alpha_for_triangle(
+    points: np.ndarray,
+    m: int,
+    n: int,
+) -> np.ndarray:
+    alpha = np.zeros((m, n))
+    for i in range(min(points[0]), max(points[0])):
+        for j in range(min(points[1]), max(points[1])):
+            x = np.linalg.solve(points, [i, j, 1])
+            if min(x) > 0:
+                alpha[i, j] = 1
+    return alpha
+
