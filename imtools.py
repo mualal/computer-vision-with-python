bcebeb88cadb905bae4e76ac31132bb6ec37224e
Alexander Muravtsev <almuravcev@yandex.ru>
2022-08-28 12:36:13 +0300

    add knn classifier

--- imtools.py
+++ imtools.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import matplotlib.pyplot as plt
 from PIL import Image
 
 
@@ -40,6 +41,41 @@ def image_averaging(
             print(image_path + ' failed to open')
             average_image /= len(images_paths)
     return np.array(average_image, 'uint8')
+
+
+def plot_2d_boundary(
+    plot_range,
+    points,
+    decisionfcn,
+    labels,
+    values=[0]
+) -> None:
+    clist = ['b', 'r', 'g', 'k', 'm', 'y']
+    x = np.arange(plot_range[0], plot_range[1], .1)
+    y = np.arange(plot_range[2], plot_range[3], .1)
+    xx, yy = np.meshgrid(x, y)
+    xxx, yyy = xx.flatten(), yy.flatten()
+    zz = np.array(decisionfcn(xxx, yyy))
+    zz = zz.reshape(xx.shape)
+    plt.contour(xx, yy, zz, values)
+    for i, _ in enumerate(points):
+        d = decisionfcn(points[i][:, 0], points[i][:, 1])
+        correct_ndx = labels[i]==d
+        incorrect_ndx = labels[i]!=d
+        plt.plot(
+            points[i][correct_ndx, 0],
+            points[i][correct_ndx, 1],
+            '*', 
+            color=clist[i]
+        )
+        plt.plot(
+            points[i][incorrect_ndx, 0],
+            points[i][incorrect_ndx, 1],
+            'o', 
+            color=clist[i]
+        )
+    plt.axis('equal')
+    plt.show()
 
 
 if __name__ == '__main__':
