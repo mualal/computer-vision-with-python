fd02afe2c91f20755c9537e8783ca36a43a2086a
Alexander Muravtsev <almuravcev@yandex.ru>
2022-08-29 14:26:17 +0300

    continue with basic image handling and processing

--- imtools.py
+++ imtools.py
@@ -7,6 +7,11 @@ from PIL import Image
 def get_imlist(
     path: str
 ) -> list:
+    """
+    получить список имён всех jpg-файлов в каталоге
+    @param path: путь к каталогу
+    @return: список имён всех jpg-файлов в каталоге
+    """
     return [os.path.join(path, f) for f in os.listdir(path) if f.endswith('.jpg') and not f.startswith('.')]
 
 
@@ -14,6 +19,12 @@ def arr_resize(
     arr: np.array,
     target_size: tuple,
 ) -> np.array:
+    """
+    изменить размер массива с помощью PIL
+    @param arr: numpy-массив
+    @param target_size: целевой размер массива
+    @return: numpy-массив с размером, приведённым к целевому
+    """
     pil_im = Image.fromarray(np.uint8(arr))
     return np.array(pil_im.resize(target_size))
 
@@ -22,10 +33,21 @@ def hist_equalization(
     arr: np.array,
     bins_count=256,
 ) -> tuple:
+    """
+    выравнивание гистограммы полутонового изображения
+    @param arr: массив изображения
+    @param bins_count: количество интервалов у гистограммы
+    @return: изображение с выровненной гистограммой и значения функции распределения
+    """
+    # получить гистограмму изображения
     arr_hist, bins = np.histogram(arr.flatten(), bins_count)
+    # функция распределения значений пикселей в изображении
     cdf = arr_hist.cumsum()
+    # (нормированная так, чтобы привести значения к требуемому диапазону)
     cdf = 255 * cdf / cdf[-1]
 
+    # использовать линейную интерполяцию функции распределения (cdf)
+    # для нахождения значений новых пикселей
     new_arr = np.interp(arr.flatten(), bins[:-1], cdf)
     return new_arr.reshape(arr.shape), cdf
 
@@ -33,23 +55,42 @@ def hist_equalization(
 def image_averaging(
     images_paths: list
 ) -> np.ndarray:
+    """
+    вычислить среднее списка изображений
+    @param images_paths: список с путями к изображениям
+    @return: среднее изображение
+    """
+    # открыть первое изображение и преобразовать
+    # в массив типа float
     average_image = np.array(Image.open(images_paths[0]), 'f')
+    success_count = len(images_paths)
     for image_path in images_paths[1:]:
         try:
             average_image += np.array(Image.open(image_path))
         except:
             print(image_path + ' failed to open')
-            average_image /= len(images_paths)
+            success_count -= 1
+    average_image /= success_count
     return np.array(average_image, 'uint8')
 
 
 def plot_2d_boundary(
-    plot_range,
-    points,
+    plot_range: tuple,
+    points: list,
     decisionfcn,
-    labels,
+    labels: np.ndarray,
     values=[0]
 ) -> None:
+    """
+    визуализировать классификацию всех тестовых точек и показать,
+    насколько хорошо классификатор разделяет 2 класса
+    @param plot_range: диапазон (xmin, xmax, ymin, ymax)
+    @param points: список точек
+    @param decisionfcn: функция, принимающая решение
+    @param labels: массив меток
+    @param values: список изолиний для отображения
+    @return: None
+    """
     clist = ['b', 'r', 'g', 'k', 'm', 'y']
     x = np.arange(plot_range[0], plot_range[1], .1)
     y = np.arange(plot_range[2], plot_range[3], .1)
--- pca.py
+++ pca.py
@@ -4,7 +4,12 @@ import numpy as np
 def pca(
     flattened_arrays: np.ndarray,
 ) -> tuple:
-
+    """
+    метод главных компонент
+    @param flattened_arrays: обучающие данные, хранящиеся в виде сглаженных массивов
+    по одному в каждой строке
+    @return: матрица проекций (наиболее важные измерения в начале), дисперсия и среднее
+    """
     # количество изображений и размерность изображения
     flattened_arrays_count, dim = flattened_arrays.shape
 
@@ -17,8 +22,8 @@ def pca(
         M = np.dot(flattened_arrays, flattened_arrays.T)  # ковариационная матрица
         eigenvalues, eigenvectors = np.linalg.eigh(M)  # собственные значения и векторы
         temp = np.dot(flattened_arrays.T, eigenvectors).T  # компактный трюк
-        vectors = temp[::-1]
-        values = np.sqrt(eigenvalues)[::-1]
+        vectors = temp[::-1]  # меняем порядок (так как исходно в порядке возрастания важности)
+        values = np.sqrt(eigenvalues)[::-1]  # меняем порядок (так как исходно в порядке возрастания важности)
 
         for i in range(vectors.shape[1]):
             vectors[:, i] /= values
